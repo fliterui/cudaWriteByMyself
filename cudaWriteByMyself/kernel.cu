4e7473a795ed24b566569dcd1ce50ef675819f66
bigShit <3289322708@qq.com>
2024-04-13 16:31:43 +0800

    1

--- cudaWriteByMyself/kernel.cu
+++ cudaWriteByMyself/kernel.cu
@@ -23,7 +23,7 @@ int main()
 	readData(signal, ori, M, N);															//读matlab的回波信号, 然后直接传给doGpuProcessing, 在那里面cudaMalloc
 	//printf("%lf", cuCreal(ori[1]));
 	float time = 0;
-	int runNum = 1000;
+	int runNum = 1;
 	for (int i = 0; i < runNum; i++)
 	{
 		time+=doGpuProcessing(signal, ori, M, N);									
@@ -34,7 +34,9 @@ int main()
 }
 
 /*warmup
-* 块和网格结构 
+* 你别说, 传数据也可以异步, 把signal分成几块, 弄多个流, 同时传是不是能快点
+* 好像cudaMalloc ori和cudaMemcpy ori可以和pc的d_signal的fft并行
+* 块和网格结构
 * 瘦块
 * 看看那些规约, 循环展开
 * 读数据(特别是转置), 还有延迟隐藏
